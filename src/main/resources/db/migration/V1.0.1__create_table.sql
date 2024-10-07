287254e4e24d1ede775754b21fb768df19a1cd77
c0623c1 <c0623c1forgit@mail1s.edu.vn>
2024-10-07 14:14:53 +0700

    init project

--- src/main/resources/db/migration/V1.0.1__create_table.sql
+++ src/main/resources/db/migration/V1.0.1__create_table.sql
@@ -3,5 +3,13 @@ CREATE TABLE user
     id       BINARY(16)   NOT NULL,
     username VARCHAR(255) NULL,
     password VARCHAR(255) NULL,
+    name     VARCHAR(255) NULL,
+    email    VARCHAR(255) NULL,
     CONSTRAINT pk_user PRIMARY KEY (id)
 );
+
+ALTER TABLE user
+    ADD CONSTRAINT uc_user_email UNIQUE (email);
+
+ALTER TABLE user
+    ADD CONSTRAINT uc_user_username UNIQUE (username);
